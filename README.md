d1e67e3ca6c524f57be2ae2f56551beb68e772c9
Arunkumarvelu <arunkumar.v@freshdesk.com>
2018-07-01 22:27:13 +0530

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# freshchat_restore_id
-Freshchat restore logic
+#Freshchat Restore Logic
+
+Prerequest:
+  1. Node js
+  2. Redis
+
+Installation:
+  1. Start redis server
+  2. npm install
+  3. npm run dev
+
+URL http://localhost:7001
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const redis          = require('redis');
+const app            = express();
+const port           = 7001;
+const path           = require('path');
+var client           = redis.createClient();
+var cookieParser     = require('cookie-parser')
+app.use('/assets', express.static(path.join(__dirname, '/app/assets')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw({
+  inflate: true,
+  limit: '100kb',
+  type: 'application/*'
+}));
+app.use(cookieParser());
+
+client.on('connect', function() {
+    require('./app/routes')(app, client);
+    app.get('/login', function(req, res) {
+      res.sendFile(path.join(__dirname + '/app/template/login/index.html'));
+    });
+    app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname + '/app/template/account/index.html'));
+    });
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });
+});
+client.on('error', function (err) {
+    console.log('In Redis Something went wrong ' + err);
+});
+
